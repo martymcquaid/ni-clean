0f4ae983a063776c7ab51c8775bb167dbb7a11b7
martymcquaid <martymcquaid@users.noreply.github.com>
2025-12-01 10:20:04 +0000

    Add src/components/Footer.tsx

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-100 py-6">
+      <div className="container mx-auto text-center text-gray-600">
+        <p>&copy; 2023 NI Clean. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
